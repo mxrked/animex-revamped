1929cabe02a42bd2ed782af2cabb1fc4fddd489b
Parker <pp101socials@gmail.com>
2022-10-13 13:28:26 -0400

    Created project

---
+++ README.md
@@ -0,0 +1,29 @@
+===
+
+# DESCRIPTION
+
+---
+
+===
+
+# DEPLOYMENT
+
+---
+
+Demo: https://idyllic-pastelito-c514d3.netlify.app/
+
+===
+
+# DEVELOPMENT
+
+---
+
+## Started Developement - 10/13/2022
+
+## Ended Development -
+
+## Status
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b6b26af0-bb5c-410e-8935-e03636702754/deploy-status)](https://app.netlify.com/sites/idyllic-pastelito-c514d3/deploys)
+
+===
---
+++ next.config.js
@@ -0,0 +1,18 @@
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  reactStrictMode: true,
+  webpack5: true,
+
+  // Fixes fs issue
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+
+  //! This added clean urls and eliminates the .html extension aswell as linking
+  //! CSS and JS with the trailingSlash
+  assetPrefix: isProd ? "https://idyllic-pastelito-c514d3.netlify.app/" : "",
+  trailingSlash: true,
+};
